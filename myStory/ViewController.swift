14b4a4d8d8842d381e062acba437adf0ac1549c2
Schadrack Cineas <sjcineas@gmail.com>
2023-02-25 01:23:43 -0500

    Initial Commit

---
+++ myStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myStory
+//
+//  Created by Schadrack Cineas on 2/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
